9e49826f2bc05931bc9ef9aa6ba11a73a0dff6fc
Lucas Possatti <lucas_possatti@hotmail.com>
2017-06-02 14:38:44 -0300

    Clean maps directory

--- maps/bmp-to-txt.sh
+++ maps/bmp-to-txt.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# $1: I believe it is an RGBA bmp file.
+# $2: Path to txt that will be produced.
+
 ## Transform colorful bitmaps, into txts that can be used by the game
 hexdump -e '20/4 " %08X" "\n"' -vs 138 $1 | perl -pe 's/000000FF/W/g; s/FFFFFFFF/./g; s/FF0000FF/S/; s/ //g' > $2
 
 # Things I've tried:
-hexdump -e '80/1 " %02X" "\n"' -vs 138 20x20-color.bmp | less
-hexdump -e '20/1 " %02X" "\n"' -vs 138 20x20-color.bmp | less
-hexdump -e '20/4 " %08X" "\n"' -vs 138 20x20-color.bmp | less
-hexdump -e '20/1 " %_u" "\n"' -vs 138 20x20-color.bmp | less
-hexdump -e '20/4 " %08X" "\n"' -vs 138 20x20-color.bmp | perl -pe 's/000000FF/W/g; s/FFFFFFFF/E/g; s/FF0000FF/S/' | less
+# hexdump -e '80/1 " %02X" "\n"' -vs 138 20x20-color.bmp | less
+# hexdump -e '20/1 " %02X" "\n"' -vs 138 20x20-color.bmp | less
+# hexdump -e '20/4 " %08X" "\n"' -vs 138 20x20-color.bmp | less
+# hexdump -e '20/1 " %_u" "\n"' -vs 138 20x20-color.bmp | less
+# hexdump -e '20/4 " %08X" "\n"' -vs 138 20x20-color.bmp | perl -pe 's/000000FF/W/g; s/FFFFFFFF/E/g; s/FF0000FF/S/' | less
